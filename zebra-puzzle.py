d7acbbd300e58cc60d559c3bd21b50bdd74c85a8
Sher Minn Chong <sherminn.chong@gmail.com>
2015-06-24 11:28:11 +0800

    Implement solver

--- zebra-puzzle.py
+++ zebra-puzzle.py
@@ -20,27 +20,47 @@ Zebra puzzle solver:
 Now, who drinks water? Who owns the zebra?
 """
 
-houses = range(5)
+import itertools 
+
+houses = range(1,6)
 
 def is_next_to(house1, house2):
 	return abs(house1 - house2) == 1
 
 def is_right_of(house1, house2):
+	""" Return true if house1 is right of house2, else false"""
 	return house2 - house1 == 1
 
 # conditions
 
-if Englishman == red #2
-if Spaniard == dog #3
-if codde == green #4
-if Ukranian == tea #5
-if is_right_of(green, ivory) #6
-if Old_Gold == snails #7
-if Kools == yellow #8
-if milk == 2 #9
-if Norwegian == 0 #10
-if is_next_to(Chesterfields, fox) #11
-if is_next_to(Kools, horse) #12
-if Lucky_Strike == orange_juice #13
-if Japanese == Parliaments #14
-if is_next_to(Norwegian, blue)
+possibilities = list(itertools.permutations(houses, len(houses)))
+
+def solve():
+	results = [(zebra, water)
+		for (red, green, ivory, yellow, blue) in possibilities
+		if is_right_of(green, ivory) #6
+		
+		for (Englishman, Spaniard, Ukranian, Norwegian, Japanese) in possibilities
+		if Englishman == red #2
+		if Norwegian == houses[0] #10
+		if is_next_to(Norwegian, blue)
+
+		for (coffee, tea, milk, orange_juice, water) in possibilities
+		if coffee == green #4
+		if Ukranian == tea #5
+		if milk == houses[len(houses)/2] #9
+		
+		for (Old_Gold, Kools, Lucky_Strike, Parliaments, Chesterfields) in possibilities
+		if Kools == yellow #8
+		if Lucky_Strike == orange_juice #13
+		if Japanese == Parliaments #14
+		
+		for (dog, snails, horse, fox, zebra) in possibilities
+		if Spaniard == dog #3
+		if Old_Gold == snails #7
+		if is_next_to(Chesterfields, fox) #11
+		if is_next_to(Kools, horse) #12
+	]
+	return results
+
+print solve()
